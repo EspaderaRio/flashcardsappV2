24fd0f54c46ef12047ed8a39abf40c54726176c8
EspaderaRio <espaderario@gmail.com>
2025-12-16 04:12:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1186,8 +1186,7 @@ window.addEventListener("appinstalled", () => {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("service-worker.js")
+    navigator.serviceWorker.register("./service-worker.js");
       .then(() => console.log("Service Worker registered"))
       .catch(err => console.error("SW registration failed", err));
   });
